74525a082ff9443e46978815a7c515c261629db0
ckeene339 <143559301+ckeene339@users.noreply.github.com>
2024-04-17 14:08:25 -0500

    Create television.py

---
+++ television.py
@@ -0,0 +1,32 @@
+class Television:
+  MIN_VOLUME = 0
+  MAX_VOLUME = 2
+  MIN_CHANNEL = 0
+  MAX_CHANNEL = 3
+
+  
+  def __init__(self):
+    pass
+
+  def power(self):
+    pass
+
+  def mute(self):
+    pass
+
+  def channel_up(self):
+    pass
+
+  def channel_down(self):
+    pass
+
+  def volume_up(self):
+    pass
+
+  def volume_down(self):
+    pass
+  
+  def __str__(self):
+    pass
+
+  
