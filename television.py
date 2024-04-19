4847bbf461306bd5d3291e593b3635a1eb625bcd
ckeene339 <143559301+ckeene339@users.noreply.github.com>
2024-04-19 17:55:38 -0500

    Update television.py

--- television.py
+++ television.py
@@ -6,8 +6,10 @@ class Television:
 
   
   def __init__(self) -> None:
+    self.__status = False
     self.__muted = False
     self.__volume = Television.MIN_VOLUME
+    self.__channel = Television.MIN_CHANNEL
 
   def power(self):
     self.__status = not self.__status
